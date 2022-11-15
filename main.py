4aaebef91d2770740a7e0db3acf77752982ef775
adriansISPC <108304345+adriansISPC@users.noreply.github.com>
2022-11-15 20:44:25 -0300

    persistencia de datos
    Se guardan graficos y resumen de estadisticos descriptivos.

--- main.py
+++ main.py
@@ -13,8 +13,12 @@ import statsmodels.api as sm
 #  CARGA Y REVISION DE DATOS - Analisis preliminar exploratorio
 # ==========================================================================================================================================================
 
-#Lectura del enlace  extraido con tenicas de web scrapping
-df_ventas = pd.read_csv("ventas.csv", header = 0)
+#Lectura del archivo (tambien se puede usar el enlace  extraido con tecnicas de web scrapping"
+try:
+    df_ventas = pd.read_csv("ventas.csv", header = 0)
+
+except:
+    print("No se pudo abrir archivo. Verificar")
 
 
 #Resumen dataframes "ventas"
@@ -43,9 +47,11 @@ df_ventas.info()
 
 #Primera Visualizacion Serie de Tiempo de Ventas
 
-#Grafico 1; Evolucion de las Ventas
+#--------------------------------------
+#Grafico 1; Evolucion de las Ventas ---
+#--------------------------------------
 
-grafico_uno = plt.plot(df_ventas["indice_tiempo"],df_ventas["ventas_precios_constantes"],label="Vtas. en Supermercados",c="red")
+plt.plot(df_ventas["indice_tiempo"],df_ventas["ventas_precios_constantes"],label="Vtas. en Supermercados",c="red")
 
 plt.style.use("dark_background")
 plt.grid(b=True,lw=0.5)
@@ -54,24 +60,27 @@ plt.title("EVOLUCION DE LAS VENTAS EN SUPERMERCADOS A PRECIOS CONSTANTES", c="re
 plt.xlabel("Anio",c="red")
 plt.ylabel("Ventas",c="red")
 
-plt.show()
+plt.savefig("grafico1.png")
 
 
 #Se observa Que es una Serie de Tiempo con Estacionalidad
-
-#Aplicando el filtro Hodrick-Prescott para separar en tendencia y componente ciclico
-
+#Aplico el filtro Hodrick-Prescott para separar en tendencia y componente ciclico
 
 df_ventas_ciclo, df_ventas_tend = sm.tsa.filters.hpfilter(df_ventas['ventas_precios_constantes'])
 df_ventas['tendencia'] = df_ventas_tend
 
-#Grafico 2, Visualizacion  componente tendencia y  componente ciclico.
 
-grafico_dos = df_ventas[["indice_tiempo","ventas_precios_constantes","tendencia"]].plot(x="indice_tiempo")
+#------------------------------------------------------------------------
+#Grafico 2, Visualizacion  componente tendencia y  componente ciclico. --
+#------------------------------------------------------------------------
+
+df_ventas[["indice_tiempo","ventas_precios_constantes","tendencia"]].plot(x="indice_tiempo")
 legend = plt.legend()
 legend.prop.set_size(14)
 plt.xlabel("Anio",c="black")
 plt.ylabel("Ventas",c="black")
+
+plt.savefig("grafico2.png")
 
 
 # ==========================================================================================================================================================
@@ -85,17 +94,21 @@ df_ventas_2022.describe()
 
 ventas_mensuales_2022= df_ventas_2022[["fecha","ventas_precios_constantes"]]
 
-#Grafico 3, Ventas Mensuales 2022
+#-------------------------------------
+#Grafico 3, Ventas Mensuales 2022 ----
+#-------------------------------------
 
-grafico_tres = plt.plot(ventas_mensuales_2022["fecha"],ventas_mensuales_2022["ventas_precios_constantes"], label ="Consumo en Supermercados Anio 2022")
+plt.plot(ventas_mensuales_2022["fecha"],ventas_mensuales_2022["ventas_precios_constantes"], label ="Consumo en Supermercados Anio 2022")
 
 plt.style.use("dark_background")
 plt.legend()
 
-plt.show()
+plt.savefig("grafico3.png")
 
 
-#Estadisticos Descriptivos
+#-----------------------------
+#Estadisticos Descriptivos ---
+#-----------------------------
 
 promedio_ventas_2022 = ventas_mensuales_2022["ventas_precios_constantes"].mean()
 
@@ -120,7 +133,9 @@ print(ventas_mensuales_2022)
 
 #Composicion_Ventas Agosto 2022, Ultimo mes de informacion
 
-#Grafico 4, Diagrama de Barras
+#---------------------------------
+#Grafico 4, Diagrama de Barras ---
+#---------------------------------
 
 eje_x_g4 = ['bebidas', 'almacen', 'panaderia', 'lacteos', 'carnes', 'verduleria_fruteria', 'alimentos_preparados_rotiseria', 'articulos_limpieza_perfumeria', 'indumentaria_calzado_textiles_hogar', 'electronicos_articulos_hogar', 'otros']
 
@@ -135,13 +150,16 @@ plt.title("Composicion del Consumo en Supermercados Agosto 2022")
 plt.xlabel("Ventas a precios constantes")
 plt.ylabel("Grupo de Articulos")
 
-plt.show()
+plt.savefig("grafico4.png")
 
 #Medios De Pago Utilizados Agosto 2022, Ultimo mes de Informacion
 
 df_medios_de_pago = df_ventas_2022[['ventas_totales_medio_pago', 'efectivo', 'tarjetas_debito', 'tarjetas_credito', 'otros_medios']]
 
-#Grafico 5, Diagrama Circular "Medios de Pago Agosto 2022"
+#------------------------------------------------------------
+#Grafico 5, Diagrama Circular "Medios de Pago Agosto 2022" --
+#------------------------------------------------------------
+
 
 lista_medios_de_pagos = ['efectivo', 'tarjetas_debito', 'tarjetas_credito', 'otros_medios']
 valores_medios_de_pago_agosto = df_ventas_2022.loc[67,['efectivo', 'tarjetas_debito', 'tarjetas_credito', 'otros_medios']]
@@ -151,7 +169,8 @@ grafico_cinco = plt.pie(valores_medios_de_pago_agosto, labels =lista_medios_de_p
 plt.title("Medios de Pago Agosto 2022")
 plt.axis("equal")
 
-plt.show()
+plt.savefig("grafico5.png")
+
 
 # ==========================================================================================================================================================
 # INFLACION AÑO 2022
@@ -163,4 +182,31 @@ df_inflacion.info()
 
 inflacion_mensual_2022= df_inflacion.loc[3:10]
 
+# ==========================================================================================================================================================
+# RESUMEN DE DATOS -- Persistencia de Datos
+# ==========================================================================================================================================================
+
+archivo_resumen_datos = open("resumen_estadisticos_descriptivos.txt","w")
+
+texto = '''--> Promedio Ventas 2022: 24427.7550 \n \n
+--> Variabilidad Ventas 2022 (Desvío Estándar): 933.0240 \n \n 
+--> Mediana Ventas 2022: 24142.7789 \n \n 
+--> Mes de Mayores Ventas 2022: Julio \n \n 
+--> Monto Total Mayores Ventas 2022: 26090.18031 \n \n 
+--> Mes de Menores Ventas 2022: Mayo \n \n 
+--> Monto Menores Ventas 2022: 23235.0963  \n \n 
+--> Variaciones Mensuales Ventas 2022 \n \n 
+id     fecha  ventas_precios_constantes  var_mensual
+60  2022-01           25464.131520          NaN
+61  2022-02           23803.202023        -0.07
+62  2022-03           24612.901369         0.03
+63  2022-04           24141.199035        -0.02
+64  2022-05           23235.096273        -0.04
+65  2022-06           24144.358859         0.04
+66  2022-07           26090.180313         0.08
+67  2022-08           23930.970811        -0.08'''
+
+archivo_resumen_datos.write(texto)
+
+archivo_resumen_datos.close()
 
